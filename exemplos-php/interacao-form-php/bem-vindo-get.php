ea0c0d5ff6683d3d4ae7fc99a456b5741bdcc48e
Lukas891 <lucasstrik89114@gmail.com>
2023-03-30 17:52:38 -0300

    Bem Vindo Get/Post +idade

--- exemplos-php/interacao-form-php/bem-vindo-get.php
+++ exemplos-php/interacao-form-php/bem-vindo-get.php
@@ -2,8 +2,10 @@
 // exemplo usando o metoto GET.
 $nome = $_GET["nome"];
 $email = $_GET["email"];
+$idade = $_GET["idade"];
 
 echo "Bem vindo ao mundo PHP! $nome <br>";
-echo "E-mail registrado $email";
+echo "E-mail registrado $email <br>";
+echo "Idade $idade"; 
 
 ?>
--- exemplos-php/interacao-form-php/bem-vindo-post.php
+++ exemplos-php/interacao-form-php/bem-vindo-post.php
@@ -2,8 +2,10 @@
 // exemplo usando o metoto POST.
 $nome = $_POST["nome"];
 $email = $_POST["email"];
+$idade = $_POST["idade"];
 
 echo "Bem vindo ao mundo PHP! $nome <br>";
-echo "E-mail registrado $email";
+echo "E-mail registrado $email <br>";
+echo "Idade $idade";
 
 ?>
