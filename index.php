58e7decb1ce2691db219ac5d3c8a134502fe67bb
Lukas891 <lucasstrik89114@gmail.com>
2023-03-30 11:52:26 -0300

    Ola Mundo PHP

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        /* tag style e um dois lugares para codificar css*/
+        h1{
+            font-family: Arial;
+            color: #ffffff;
+            background-color: black;
+            text-align: center;
+        }
+    </style>
+    <h1 onclick="alert('Ola mundo JS')">Minha pagina</h1>
+    <script>
+        document.write("Ola mundo JS 2")
+    </script>
+    <?php
+        echo "Ratchoflais";
+    ?>
+</body>
+</html>
