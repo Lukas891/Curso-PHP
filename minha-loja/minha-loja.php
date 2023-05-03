75377b26fb05347a9c3da5c7dd9f04b6deaa935d
Lukas891 <lucasstrik89114@gmail.com>
2023-05-03 18:36:09 -0300

    Loja Vitual de Doces (Apenas html)

---
+++ minha-loja/minha-loja.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Doces Dona Nita</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <style>
+    a {
+      float: right;
+    }
+
+    .card {
+      float: left;
+      margin: 10px;
+      width: 300px;
+    }
+
+    h2 {
+      text-align: center;
+    }
+  </style>
+  <div class="container">
+    <h2>Doces Dona Nita</h2>
+    <div class="card">
+      <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/nutela.webp">
+      <div class="card-body">
+        <h4 class="card-title">Nutella e Ninho 🍫🌰</h4>
+        <h6 class="card-text">R$: 39,90 <a href="#" class="btn btn-primary btn-sm">🛒</a></h6>
+      </div>
+    </div>
+    <div class="card">
+      <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-limao.webp">
+      <div class="card-body">
+        <h4 class="card-title">Brownnie e Limao 🍋🎂</h4>
+        <h6 class="card-text">R$: 39,90 <a href="#" class="btn btn-primary btn-sm">🛒</a></h6>
+      </div>
+    </div>
+    <div class="card">
+      <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-maracuja.webp">
+      <div class="card-body">
+        <h4 class="card-title">Brigadeiro e Maracuja </h4>
+        <h6 class="card-text">R$: 39,90 <a href="#" class="btn btn-primary btn-sm">🛒</a></h6>
+      </div>
+    </div>
+    <div class="card">
+      <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/chocolate-oreo.webp">
+      <div class="card-body">
+        <h4 class="card-title">Chocolate e Oreo</h4>
+        <h6>R$: 39,90 <a href="#" class="btn btn-primary btn-sm">🛒</a></h6>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
