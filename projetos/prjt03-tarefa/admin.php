fb7ecc129bb64e7ca41eb950393f679734f84dcc
Lukas891 <lucasstrik89114@gmail.com>
2023-05-20 21:56:24 -0300

    Ataulizações Projeto 03

---
+++ projetos/prjt03-tarefa/admin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tarefas</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <style>
+        h2 {
+            text-align: center;
+        }
+    </style>
+    <div class="container mt-3">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <br>
+                <h2>Lista de tarefas</h2><br>
+                <table class="table table-striped">
+                    <tr>
+                        <th>Tarefa</th>
+                        <th class="col-sm-1">Editar</th>
+                        <th class="col-sm-1">Excluir</th>
+                    </tr>
+                    <?php
+                    include "conectar.php";
+                    $retorno = conectar("select * from tarefa;");
+                    while ($linha = $retorno->fetch_assoc()) {
+                        $id = $linha['id'];
+                        $nome = $linha['nome'];
+                        echo "<tr>
+                <td>$nome</td>
+                <td class='text-center'><a href='editar-tarefa.php?id-tarefa=$nome'class='btn btn-outline-primary btn-sm'>✏</a></td>
+                <td class='text-center'><a href='apagar-tarefa.php?id-tarefa=$id'class='btn btn-outline-danger btn-sm'>🗑</a></td>
+                </tr>";
+                    }
+                    ?>
+                </table>
+                <a href="criar-tarefa.php" class="btn btn-warning">Nova tarefa</a>
+                <a href="destroy.php" class="btn btn-danger">Sair</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ projetos/prjt03-tarefa/form-nova-tarefa.php
@@ -0,0 +1,7 @@
+<form action="criar-tarefa.php" method="GET">
+    <label for="nome-tarefa" class="form-label">Qual o nome da tarefa?</label>
+    <input type="text" class="form-control" name="nome-tarefa" placeholder="Nome da tarefa" required><br>
+    <button type="submit" class="btn btn-success">Registrar tarefa</button>
+    <a href='admin.php' class='btn btn-danger'>Cancelar</a>
+</form>
+    
